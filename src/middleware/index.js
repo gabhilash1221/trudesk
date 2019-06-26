f5e71748153d91e6d16169c1f5fe55b46c59daf7
gabhilash1221 <51361550+gabhilash1221@users.noreply.github.com>
2019-06-26 10:46:30 +0530

    For ticket field update
    For ticket field update

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -145,9 +145,10 @@ module.exports = function (app, db, callback) {
 function allowCrossDomain (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,accesstoken,X-RToken,X-Token'
+    'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,accesstoken,X-RToken,X-Token,Access-Control-Allow-Origin'
   )
 
   if (req.method === 'OPTIONS') {
